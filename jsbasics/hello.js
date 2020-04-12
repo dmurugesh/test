4a7a776a8a326009aa429bc8e81f9d5592f6952f
dmurugesh <dmurugeshkhanna@gmail.com>
2020-04-12 10:56:50 +0530

    plaindrome

--- jsbasics/hello.js
+++ jsbasics/hello.js
@@ -1,1 +1,19 @@
-console.log("Helloworld");
+let readline = require("readline-sync");
+
+let str = readline.question("enter word :");
+
+
+function palindrome(str) {
+
+   str = str.toLowerCase().replace(/[^a-z]+/g,""); // ,"" remove spaces 
+
+   if (str === str.split("").reverse().join(""))
+   {
+        console.log("Palindrome number");
+        } 
+        else { 
+            console.log("Not Palindrome number"); 
+           }
+   }
+
+palindrome(str);
