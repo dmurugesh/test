18c5ccee4d65050f5767441e3d395ae752173a53
dmurugesh <dmurugeshkhanna@gmail.com>
2020-06-22 00:06:35 +0530

    section1

---
+++ 02_js_fundamentals/007_variables.js
@@ -0,0 +1,36 @@
+// var, let, const
+
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve smith'
+// console.log(name);
+
+// //init var 
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting); 
+
+
+//LET
+// let name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name); 
+
+// CONST 
+// const name = 'John';
+// console.log(name);
+// name = 'Sara';
+
+const person = {
+    name: 'John',
+    age: 30
+}
+person.name = 'Sara';
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+
+console.log(numbers);
