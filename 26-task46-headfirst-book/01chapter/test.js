717107536f34edca3962c917f7bc26b46f709f85
dmurugesh <dmurugeshkhanna@gmail.com>
2020-08-01 20:49:20 +0530

    testing chapter one and two

---
+++ 26-task46-headfirst-book/01chapter/test.js
@@ -0,0 +1,80 @@
+
+//Assiging different types of variables
+// var winners = 2;
+// var name = "Duke";
+
+// var losers;
+// var isEligible = false;
+
+// Test for jokes 1
+
+// var joke = "JavaScript walked into a bar....";
+// var toldJoke = false;
+// var $punchline =
+//  "Better watch out for those semi-colons.";
+// var entage = 20;
+// var result;
+// if (toldJoke === false) {
+//  alert($punchline);
+// } else {
+//  alert(joke);
+// }
+
+// Test for jokes 2
+
+//  Movie Night
+// var zipCode = 98104;
+// var joesFavoriteMovie = 'Forbidden Planet';
+// var movieTicket$ = 9;
+// if (movieTicket$ >= 9) {
+//  alert("Too much!");
+// } else {
+//  alert("We're going to see " + joesFavoriteMovie);
+// }
+
+// Print star using while loop
+var scoops = 5;
+// let a = '*';
+// while (scoops < 5) {
+//  document.write(`${a}<br>`);
+//  a = a + '*';
+//  scoops = scoops + 1;
+// }
+// document.write("Life without ice cream isn't the same");
+
+
+// If else if satements
+// if (scoops < 3) {
+//     alert("Eat faster, the  icecream is going to melt");
+// } else if (scoops > 3) {
+//     alert("Icecream is running low!");
+// }
+
+
+//Code magnet 
+// var  i = 0;
+// var name = "Joe";
+// while (i < 2) {
+//     document.write("Happy Birthday to you.<br>");
+//     i = i + 1;
+// }
+// document.write("Happy Birthday dear " + name + ",<br>");
+// document.write("Happy Birthday to you.<br>");
+
+
+var word = "bottles";
+var count = 99;
+while (count > 0) {
+ console.log(count + " " + word + " of beer on the wall");
+ console.log(count + " " + word + " of beer,");
+ console.log("Take one down, pass it around,");
+ count = count - 1;
+ if (count > 0) {
+ console.log(count + " " + word + " of beer on the wall.");
+ } else {
+ console.log("No more " + word + " of beer on the wall.");
+ }
+}
+
+
+
